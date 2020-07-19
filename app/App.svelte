deeed62fbaaa4270d468dfa0cfd1ab7a964e960d
mrsauravsahu <mrsauravsahu@outlook.com>
2020-07-19 13:53:30 +0530

    add tabs for todo and completed

--- app/App.svelte
+++ app/App.svelte
@@ -1,24 +1,23 @@
 <page>
-    <actionBar title="Svelte Native App" />
-    <gridLayout>
-        <label class="info" horizontalAlignment="center" verticalAlignment="center" textWrap="true">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
+    <actionBar title="Your Todos"/>
+    
+    <tabs tabsPosition="bottom">
+        <tabStrip>
+            <tabStripItem title="Todos" />
+            <tabStripItem title="Completed" />
+        </tabStrip>
+
+        <tabContentItem>
+            <label textWrap="true">These are still yet to do. Check 'em off.</label>
+        </tabContentItem>
+        <tabContentItem>
+            <label textWrap="true">These are your completed todos. Great work!</label>
+        </tabContentItem>
+    </tabs>
 </page>
 
-<script>
-    let message = "Blank Svelte Native App"
-</script>
-
 <style>
-    .info .fas {
-        color: #3A53FF;
-    }
-    .info {
-        font-size: 20;
+    label {
+        margin: 8;
     }
 </style>
